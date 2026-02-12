81e459c03520a9aba52a82f774464104fc20fcca
Kucherbar <oooarem@gmail.com>
2026-02-12 17:50:28 +0300

    first commit

---
+++ src/Mycar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Mycar {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 14;
+        int z = 16;
+        int sum = x+y;
+
+        System.out.println("Hello world");
+        System.out.println(x);
+        System.out.println(x + y);
+        System.out.println(sum);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello" + name);
+
+    }
+}
---
+++ src/game.java
@@ -0,0 +1,17 @@
+public class game {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int step = 0;
+        int personX;
+        int personY;
+        int personLive = 3;
+        int sizeBoard = 5;
+        personX = 1 + sizeBoard / 2;
+        personY = 1 + sizeBoard / 2;
+       step += 1;
+        step -= 10; // эквивалентно: step = step - 10
+        step *= 5; // эквивалентно: step = step * 5
+        step /= 2; // эквивалентно: step = step / 2
+
+    }
+}
